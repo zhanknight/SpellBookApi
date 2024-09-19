1788af6694ac79154d6b54205af01be3e72d6cda
zhanknight <20487008+zhanknight@users.noreply.github.com>
2024-09-19 12:54:01 -0400

    ReagentCount on SpellView

--- Models/Views/SpellView.cs
+++ Models/Views/SpellView.cs
@@ -4,5 +4,6 @@ public class SpellView
 {
     public Guid Id { get; set; }
     public required string Name { get; set; } = string.Empty;
+    public int ReagentCount { get { return Reagents.Count; } }   
     public ICollection<ReagentView> Reagents { get; set; } = new List<ReagentView>();
 }
